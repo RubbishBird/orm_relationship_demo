f44930766b63a0b952786955144073b998008221
RubbishBird <1158784496@qq.com>
2018-11-22 08:24:41 +0800

    设置另外一个表外键

--- article/models.py
+++ article/models.py
@@ -4,7 +4,10 @@ class Category(models.Model):
     category = models.CharField(max_length=100)
 
 class Article(models.Model):
+    #定义数据库字段
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+    #设置表的外键
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    username = models.ForeignKey('frontuser.FrontUser',on_delete=models.CASCADE,null=True)
 
